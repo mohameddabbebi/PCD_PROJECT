f98e343f51a4598f8558c7f8a92a660fb08ff6c4
mohameddabbebi <154708205+mohameddabbebi@users.noreply.github.com>
2025-02-12 12:07:32 +0100

    Update text_pre_process.py

--- text_pre_process.py
+++ text_pre_process.py
@@ -4,12 +4,12 @@ romans = [
     'I', 'V', 'X'
 ]
 
-with open("/content/file.txt", "r") as file:
+with open("/content/file1.txt", "r") as file:
     lines = file.readlines()
 
 str1 = ""  # Clé (titre)
 str2 = ""  # Valeur (contenu)
-
+map2 = {}
 for line in lines:
     line = line.strip()  # Supprime les espaces et sauts de ligne
 
@@ -17,11 +17,16 @@ for line in lines:
         continue
 
     first_word = line.split()[0]  # Premier mot de la ligne
-
+    tst=0
+    for x in first_word :
+        if(x.isalpha() and x not in romans) :
+          tst+=1
     # Vérifie si la ligne commence par un nombre romain
-    if first_word[0] in romans:
+    if first_word[0] in romans and tst==0 :
         if str1:  # Sauvegarde l'ancienne section avant d'en commencer une nouvelle
             mp[str1] = str2.strip()
+            
+        map2[first_word] = line
         str1 = line  # Nouveau titre
         str2 = ""  # Réinitialise le contenu
     else:
