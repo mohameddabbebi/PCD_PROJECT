bfc2ab60fa3a906234f9fdc2ce8bed5e0cc3c094
RamiBenAmor <155146849+RamiBenAmor@users.noreply.github.com>
2025-04-11 21:56:21 +0100

    Delete count.py

--- count.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-def count_pdf_pages(pdf_path):
-    with fitz.open(pdf_path) as doc:
-        return len(doc)
-
-# Exemple d'utilisation
-pdf_file = "document.pdf"
-print(f"Nombre de pages : {count_pdf_pages(pdf_file)}")
